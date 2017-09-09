5f03a31f83f139c01b5c000c70bf38b9a3e3e5a3
Libby <luckycomet25@gmail.com>
2017-09-09 15:36:08 -0400

    Simple Shift
    Added beginning to Pedometer, will add timer

--- app/src/main/java/teamwork/pennapps16/main_activity.java
+++ app/src/main/java/teamwork/pennapps16/main_activity.java
@@ -1,11 +1,18 @@
 package teamwork.pennapps16;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.CountDownTimer;
 import android.provider.AlarmClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.telecom.Call;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.View;
@@ -15,6 +22,7 @@ import android.widget.Chronometer;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Button;
+
 import java.util.List;
 import java.util.ArrayList;
 import android.view.inputmethod.*;
@@ -25,22 +33,55 @@ import java.lang.Object;
 import android.provider.AlarmClock;
 import android.content.SharedPreferences;
 import java.io.File;
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+import com.facebook.*;
+import com.facebook.login.widget.LoginButton;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+
+import org.w3c.dom.Text;
+
+import java.util.Arrays;
+
+
+
+
+
 
 
 /**
  * Created by Libby on 9/9/17.
  */
 
-public class main_activity extends AppCompatActivity {
+public class main_activity extends AppCompatActivity implements SensorEventListener {
+    //CallbackManager callbackManager;
+
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+        //FacebookSdk.sdkInitialize(getApplicationContext());
         setContentView(R.layout.main_layout);
+        super.onCreate(savedInstanceState);
+
+        final TextView pedometer = (TextView) findViewById(R.id.pedometer_text);
+
+
+       // final LoginButton loginButton = (LoginButton) findViewById(R.id.login_button);
+        //callbackManager = CallbackManager.Factory.create();
+        //final TextView loginReport = (TextView) findViewById(R.id.login_log) ;
+
+
+
+
+
+
         final TextView Title = (TextView) findViewById(R.id.title_main);
         final Button StartButton = (Button) findViewById(R.id.start_button);
+        final Button logIn = (Button) findViewById(R.id.login_button);
             StartButton.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
                     setContentView(R.layout.select_layout);
+
                     final Button GameChoose = (Button) findViewById(R.id.game_init);
                     final Button StatButton = (Button) findViewById(R.id.stat_button);
                     final Button CustomButton = (Button) findViewById(R.id.custom_button);
@@ -48,6 +89,34 @@ public class main_activity extends AppCompatActivity {
                         @Override
                         public void onClick(View v) {
                             setContentView(R.layout.game_layout);
+                            Button challButton = (Button) findViewById(R.id.challenge_start);
+                            final SensorManager sensorManager;
+                            sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+
+                            challButton.setOnClickListener(new View.OnClickListener() {
+                                @Override
+                                public void onClick(View v) {
+                                    Sensor countSensor = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
+                                    if(countSensor!=null){
+
+                                        sensorManager.registerListener(new SensorEventListener() {
+                                            @Override
+                                            public void onSensorChanged(SensorEvent event) {
+                                                pedometer.setText(String.valueOf(event.values[0]));
+                                            }
+
+                                            @Override
+                                            public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+                                            }
+                                        }, countSensor, SensorManager.SENSOR_DELAY_UI);
+
+
+
+                                    }
+                                }
+                            });
+
                         }
 
                     });
@@ -63,7 +132,34 @@ public class main_activity extends AppCompatActivity {
                             setContentView(R.layout.custom_layout);
                         }
                     });
+
                 }
+
+
+
+
+
+
+
+
+
             });
     }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+
+
+   /* @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }*/
 }
